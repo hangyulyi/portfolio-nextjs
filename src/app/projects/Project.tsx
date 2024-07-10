970bce4de5d159fc27c3f177edcb33526cd2e366
Hangyul Yi <hangyulyi3@gmail.com>
2024-07-09 23:39:01 -0600

    update projects slug

--- src/app/projects/Project.tsx
+++ src/app/projects/Project.tsx
@@ -5,8 +5,19 @@ export interface Project {
     slug: string;
     technologies: string[];
     link?: string;
+    img?: string;
+    code: boolean;
 }
 
 export const projects: Project[] = [
-    { id: 1, name: "Flower Image Classifier", description: "Train an image classifier to classify different species of flowers", slug: "flower-classifier", technologies: ["Python", "PyTorch"], link: "https://github.com/hangyulyi/Image-Classifier" }
+    { 
+        id: 1, 
+        name: "Flower Image Classifier", 
+        description: "Train an image classifier to classify different species of flowers", 
+        slug: "flower-classifier", 
+        technologies: ["Python", "PyTorch"], 
+        link: "https://github.com/hangyulyi/Image-Classifier",
+        img: "https://hangyulyi.github.io/assets/flower_classifier.png",
+        code: true
+    }
 ]
--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { FaCode } from "react-icons/fa";
+
 import { Project, projects } from "../Project"
 
 export default function ProjectPost({ params }: { params: { slug: string } }) {
@@ -11,19 +13,21 @@ export default function ProjectPost({ params }: { params: { slug: string } }) {
     }
 
     return (
-        <div className="p-10 max-w-[2250px] mx-auto mt-10 pb-0">
+        <div className="p-10 max-w-[2000px] mx-auto mt-10 pb-0">
             <div className="bg-transparent mx-5 items-center justify-center md:flex">
-                <div className="p-0 -mx-5">
-                    {/* img- host */}
-                    <img></img>
-                </div>
                 <section className="md:w-6/12">
                     <h1 className="font-bold text-4xl pt-2 pb-10 tracking-wider">{project.name}</h1>
-                    <div>
-                        <p>{project.description}</p>
+                    <div >
+                        <p className="sm:pt-0">{project.description}</p>
                     </div>
                 </section>
+                <div className="p-0 mx-5">
+                    {/* img- host */}
+                    <img src={project.img} className="rounded-md"></img>
+                </div>
             </div>
+
+
         </div>
     )
 }
