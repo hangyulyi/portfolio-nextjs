cc3745b3212731b62113bf1fc44dc0d0bbc7b566
Hangyul Yi <hangyulyi3@gmail.com>
2024-08-18 03:10:29 -0600

    implement macbook toolbar behavior on monitoritem

--- src/app/components/Home/MonitorBar.tsx
+++ src/app/components/Home/MonitorBar.tsx
@@ -1,16 +1,50 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 
 import MonitorBarItem from "./MonitorBarItem";
 
 export default function MonitorBar() {
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
+
+    const handleMouseEnter = (index: number) => {
+        setHoveredIndex(index)
+    }
+
+    const handleMouseLeave = () => {
+        setHoveredIndex(null)
+    }
+
+
     return(
         <div className="backdrop-blur-md flex items-center">
             <div className="rounded-[12px] h-4/5 w-16 flex flex-col items-center py-2 border-zinc-400 border-solid border">
 
                 <div className="space-y-3">
-                    <MonitorBarItem name="About me" />
-                    <MonitorBarItem name="About me" />
-                    <MonitorBarItem name="About me" />
+
+                    {/* imitate behavior on macbook toolbar */}
+                    {['About me', 'My skills', 'Github', 'LinkedIn'].map((name, index) => {
+                        let scaleClass = 'scale-100';
+                        if (hoveredIndex === index) {
+                            scaleClass = 'scale-125'
+                        } else if (hoveredIndex === index - 1 || hoveredIndex === index + 1) {
+                            scaleClass = 'scale-105'
+                        }
+
+                        return (
+                            <div
+                                key={index}
+                                onMouseEnter={() => handleMouseEnter(index)}
+                                onMouseLeave={handleMouseLeave}
+                            >
+                                <MonitorBarItem 
+                                    name={name}
+                                    scaleClass={scaleClass}
+                                    isHovered={hoveredIndex === index}
+                                />
+                            </div>
+                        )
+                    })}
                 </div>
 
             </div>
--- src/app/components/Home/MonitorBarItem.tsx
+++ src/app/components/Home/MonitorBarItem.tsx
@@ -1,11 +1,22 @@
 export interface MonitorBarItemProps {
     name: string;
+    isHovered: boolean;
+    scaleClass: string;
+    // background: string;
 }
 
-const MonitorBarItem: React.FC<MonitorBarItemProps> = ({ name }) => {
+const MonitorBarItem: React.FC<MonitorBarItemProps> = ({ name, isHovered, scaleClass }) => {
     return(
-        <div>
-            <div className="w-11 rounded-[10px] aspect-square bg-white transition hover:scale-125">
+        <div className="relative">
+            <div 
+                className={`w-11 rounded-[10px] aspect-square bg-white transition-transform ${scaleClass}`}
+            >
+
+                {isHovered && (
+                    <div className="absolute w-fit left-full ml-2 px-2 py-1 bg-gray-200 rounded">
+                        {name}
+                    </div>
+                )}
             </div>
         </div>
     )
