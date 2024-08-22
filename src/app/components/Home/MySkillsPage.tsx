f957f3c412dc36d8212df18d89092d2b1a0c2a39
Hangyul Yi <hangyulyi3@gmail.com>
2024-08-22 03:06:56 -0600

    add icons to inner page

--- src/app/components/Home/MySkillsPage.tsx
+++ src/app/components/Home/MySkillsPage.tsx
@@ -1,8 +1,19 @@
+import { IoIosClose } from "react-icons/io";
+import { VscChromeMinimize } from "react-icons/vsc";
+import { IoIosResize } from "react-icons/io";
+
 export default function MySkillsPage() {
     return(
         
-        <div className="aspect-[3/2] bg-white w-full rounded-lg mx-10">
+        <div className="aspect-[3/2] bg-white w-full h-full rounded-lg mx-10 overflow-hidden relative">
+            <div className="bg-gray-800 absolute inset-x-0 top-0 h-8">
+                <div className="absolute inset-y-0 left-0 px-2 flex items-center space-x-1">
+                    <IoIosClose color="#450a0a" className="rounded-full bg-red-600" />
+                    <VscChromeMinimize color="#a16207" className="rounded-full bg-amber-400" />
+                    <IoIosResize color="#052e16" className="rounded-full bg-emerald-500" />
+                </div>
 
+            </div>
         </div>
         
     )
