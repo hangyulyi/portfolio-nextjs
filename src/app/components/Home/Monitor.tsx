40c22654704ec6a4fa006849f66da9d2a329b74b
Hangyul Yi <hangyulyi3@gmail.com>
2025-09-26 09:51:46 -0600

    update resume for 2026

--- src/app/components/Home/Monitor.tsx
+++ src/app/components/Home/Monitor.tsx
@@ -30,7 +30,7 @@ export default function Monitor () {
         } else if (name === 'LinkedIn') {
             window.open('https://www.linkedin.com/in/yihangyul/', '_blank')
         } else if (name === 'Resume') {
-            window.open('/assets/Hangyul-Yi_Resume(2025).pdf', '_blank')
+            window.open('/assets/Hangyul-Yi_Resume(2026).pdf', '_blank')
         }
     }
 
